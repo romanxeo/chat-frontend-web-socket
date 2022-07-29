d9676b6dc3aafa71162b7cdd1d327925b35217a8
romanxeo <romanxeo@gmail.com>
2022-07-30 02:28:36 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.module.css';
 
 function App() {
+
+  const [messagesList, setMessagesList] = useState([
+    {message: "Hello", id: 1, user: {id: 3, name: "Roman"}},
+    {message: "Buy", id: 2, user: {id: 4, name: "Vasya"}}
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Init</div>
     </div>
   );
 }
